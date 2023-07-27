7f28515f4af5b655ac55ef2f43948fd891c5d8fa
SpandanPan <83960135+SpandanPan@users.noreply.github.com>
2023-07-27 12:11:54 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets,transforms
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import os
+import numpy as np
+from torch_lr_finder import LRFinder
+
+
+train_transforms = A.Compose([
+    A.Normalize (mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
+    #A.ShiftScaleRotate(shift_limit = 0.2, scale_limit = 0.1, rotate_limit = 15,p=0.4),
+    #A.HorizontalFlip(),
+    A.PadIfNeeded(min_height=40, min_width=40, always_apply=True,p=0.5),
+    A.RandomCrop(height=32, width=32, always_apply=True,p=0.5,),
+    A.CoarseDropout(max_holes=1, max_height=8, max_width=8, fill_value=0, mask_fill_value=None, always_apply=False, p=0.5),
+    ToTensorV2()
+])
+
+#Test Phase transformations
+test_transforms = A.Compose([
+                           
+                             A.Normalize (mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
+                             ToTensorV2()
+
+
+# Model Scheduler
+model = resnet().to(device)
+optimizer=optim.Adam(model.parameters(),lr=0.03,weight_decay=1e-4)
+criterion = nn.CrossEntropyLoss()
+Lr_Finder=LRFinder(model,optimizer,criterion,device='cuda')
