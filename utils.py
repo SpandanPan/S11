af19c493f8b79c5fc31fb2e585fc5780ad9b8d95
SpandanPan <83960135+SpandanPan@users.noreply.github.com>
2023-07-29 01:27:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,25 +6,24 @@ import torch.optim as optim
 from torchvision import datasets,transforms
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-import os
 import numpy as np
-from torch_lr_finder import LRFinder
 from torch.optim.lr_scheduler import OneCycleLR
-from Models import resnet
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+
+from Models.resnet import ResNet18
 
 train_transforms = A.Compose([
     A.Normalize (mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
-    #A.ShiftScaleRotate(shift_limit = 0.2, scale_limit = 0.1, rotate_limit = 15,p=0.4),
-    #A.HorizontalFlip(),
-    A.PadIfNeeded(min_height=40, min_width=40, always_apply=True,p=0.5),
+    A.PadIfNeeded(min_height=48, min_width=48, always_apply=True,p=0.5),
     A.RandomCrop(height=32, width=32, always_apply=True,p=0.5,),
-    A.CoarseDropout(max_holes=1, max_height=8, max_width=8, fill_value=0, mask_fill_value=None, always_apply=False, p=0.5),
+    A.CoarseDropout(max_holes=1, max_height=16, max_width=16, fill_value=0, mask_fill_value=None, always_apply=False, p=0.5),
     ToTensorV2()
 ])
 
 #Test Phase transformations
 test_transforms = A.Compose([
-                           
                              A.Normalize (mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
                              ToTensorV2()
 
@@ -61,9 +60,117 @@ scheduler = OneCycleLR(optimizer,
                        anneal_strategy='linear')
 
 
-# Gradcam
+# Define test and train functions
 
-from pytorch_grad_cam import GradCAM
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-from pytorch_grad_cam.utils.image import show_cam_on_image
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+def train(model, device, train_loader, optimizer, criterion):
+  model.train()
+  pbar = tqdm(train_loader)
+
+  train_loss = 0
+  correct = 0
+  processed = 0
+
+  for batch_idx, (data, target) in enumerate(pbar):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+
+    # Predict
+    pred = model(data)
+
+    # Calculate loss
+    loss = criterion(pred, target)
+    train_loss+=loss.item()
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+
+    correct += GetCorrectPredCount(pred, target)
+    processed += len(data)
+
+    pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+  train_acc.append(100*correct/processed)
+  train_losses.append(train_loss/len(train_loader))
+
+def test(model, device, test_loader):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+
+            correct += GetCorrectPredCount(output, target)
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+from torch.optim.lr_scheduler import OneCycleLR
+EPOCHS=20
+scheduler = OneCycleLR(optimizer,
+                       max_lr=2.47E-02,
+                       steps_per_epoch=len(train_loader),
+                       epochs=EPOCHS,
+                       pct_start=5/EPOCHS,
+                       div_factor=100,
+                       three_phase=False,
+                       final_div_factor=100,
+                       anneal_strategy='linear')
+
+
+def misclassified_image(test_loader,device,model,train_data):
+
+  cnt=0
+  data, target = next(iter(test_loader))
+  data, target = data.to(device), target.to(device)
+  output = model(data)
+  pred = output.argmax(dim=1, keepdim=True)
+  img_lst=[]
+  img_tensor=[]
+  cat_lst=[]
+  x_lst=[]
+  for i in range(0,127):
+    x = random.randint(0,127)
+
+    if pred[x].item()!=target[x].item():
+      img1=data[x]
+      img=data[x].permute(1, 2, 0).cpu().numpy()
+      img_lst.append(img)
+      img_tensor.append(img1)
+      cat=[value for key,value in enumerate(train_data.class_to_idx) if key==target[x].item()][0]
+      cat_lst.append(cat)
+      x_lst.append(x)
+      cnt+=1
+    if cnt>9:
+      break
+  return img_lst,img_tensor,cat_lst
+
+
+invTrans = transforms.Compose([ transforms.Normalize(mean = [ 0., 0., 0. ],
+                                                     std = [ 1/0.2023, 1/0.1994, 1/0.2010 ]),
+                                transforms.Normalize(mean = [ -0.4914, -0.4822, -0.4465 ],
+                                                     std = [ 1., 1., 1. ]),
+                               ])
 
