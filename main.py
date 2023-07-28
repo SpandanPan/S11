3e4374a8fee9fb042a39342fd78a2090e61cd6ee
SpandanPan <83960135+SpandanPan@users.noreply.github.com>
2023-07-29 02:41:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from torchsummary import summary
 import matplotlib.pyplot as plt
-from resnet import ResNet18
+from Models.resnet import ResNet18
 from utils import train_transforms,test_transforms,Cifar10SearchDataset,scheduler,grad_cam,invTrans,misclassified_image,train,test
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
